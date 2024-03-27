07fff01e0e1e56098a0537407a53aba920f550d9
drrvphd <rv.phd@outlook.com>
2024-03-27 17:24:17 +1100

    modified to rakesh gcloud storage, project id and project name

--- Section3-CloudBuild-CICD/cloudrun-app-cloudbuild/gcloud-permission-commands.sh
+++ Section3-CloudBuild-CICD/cloudrun-app-cloudbuild/gcloud-permission-commands.sh
@@ -1,8 +1,6 @@
 # Assign Service account user role to the service account 
-gcloud projects add-iam-policy-binding udemy-mlops-395416 \
---member=serviceAccount:49229966525@cloudbuild.gserviceaccount.com --role=roles/iam.serviceAccountUser
+gcloud projects add-iam-policy-binding mlops-trial1 --member=serviceAccount:331369901788@cloudbuild.gserviceaccount.com --role=roles/iam.serviceAccountUser
 
 
 # Assign Cloud Run role to the service account 
-gcloud projects add-iam-policy-binding udemy-mlops-395416 \
-  --member=serviceAccount:49229966525@cloudbuild.gserviceaccount.com --role=roles/run.admin
+gcloud projects add-iam-policy-binding mlops-trial1 --member=serviceAccount:331369901788@cloudbuild.gserviceaccount.com --role=roles/run.admin
--- Section3-CloudBuild-CICD/cloudrun-app/docker-cloudrun-commands.sh
+++ Section3-CloudBuild-CICD/cloudrun-app/docker-cloudrun-commands.sh
@@ -2,16 +2,14 @@
 docker build -t demo-flask-app .
 
 # Push to Container Registry 
-docker tag demo-flask-app gcr.io/udemy-mlops-395416/demo-flask-app
-docker push gcr.io/udemy-mlops-395416/demo-flask-app
+docker tag demo-flask-app gcr.io/mlops-trial1/demo-flask-app
+docker push gcr.io/mlops-trial1/demo-flask-app
 
-gcloud run deploy demo-flask-app --image gcr.io/udemy-mlops-395416/demo-flask-app --region us-central1
+gcloud run deploy demo-flask-app --image gcr.io/mlops-trial1/demo-flask-app --region us-central1
 
 
 # Push to Artifact Registry 
-docker tag demo-flask-app us-central1-docker.pkg.dev/udemy-mlops-395416/python-apps/demo-flask-app
-docker push us-central1-docker.pkg.dev/udemy-mlops-395416/python-apps/demo-flask-app
+docker tag demo-flask-app us-central1-docker.pkg.dev/mlops-trial1/python-apps/demo-flask-app
+docker push us-central1-docker.pkg.dev/mlops-trial1/python-apps/demo-flask-app
 
-gcloud run deploy demo-flask-app2 \
---image us-central1-docker.pkg.dev/udemy-mlops-395416/python-apps/demo-flask-app \
---region us-central1
+gcloud run deploy demo-flask-app2 --image us-central1-docker.pkg.dev/mlops-trial1/python-apps/demo-flask-app --region us-central1
